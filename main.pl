1ace8d0c40e9cf35f687e0f345612565603a2b89
Gan Shen <gshen42@ucsc.edu>
2020-03-10 23:29:57 -0700

    add integers and lists

--- main.pl
+++ main.pl
@@ -135,6 +135,10 @@ query: [] ⊢ fix(lam(f, var(f))) : Ty.
     Γ ⊢ T1 : tyBool,
     Γ ⊢ T2 : tyBool.
 
+Γ ⊢ or(T1, T2) : tyBool :-
+    Γ ⊢ T1 : tyBool,
+    Γ ⊢ T2 : tyBool.
+
 Γ ⊢ if(T1, T2, T3) : Ty :-
     Γ ⊢ T1 : tyBool,
     Γ ⊢ T2 : Ty,
@@ -148,6 +152,93 @@ query: [] ⊢  and(true, false) : Ty.
 
 term: λx. if (fst x) then (snd x) else unit
 query: [] ⊢ lam(x, if(fst(var(x)), snd(var(x)), unit)) : Ty.
+*/
+
+/* Natural Numbers */
+
+Γ ⊢ 0 : tyNat.
+
+Γ ⊢ succ(T) : tyNat :-
+    Γ ⊢ T : tyNat.
+
+Γ ⊢ T1 + T2 : tyNat :-
+    Γ ⊢ T1 : tyNat,
+    Γ ⊢ T2 : tyNat.
+
+Γ ⊢ T1 - T2 : tyNat :-
+    Γ ⊢ T1 : tyNat,
+    Γ ⊢ T2 : tyNat.
+
+Γ ⊢ T1 * T2 : tyNat :-
+    Γ ⊢ T1 : tyNat,
+    Γ ⊢ T2 : tyNat.
+
+Γ ⊢ T1 / T2 : tyNat :-
+    Γ ⊢ T1 : tyNat,
+    Γ ⊢ T2 : tyNat.
+
+Γ ⊢ T1 == T2 : tyBool :-
+    Γ ⊢ T1 : tyNat,
+    Γ ⊢ T2 : tyNat.
+
+Γ ⊢ T1 > T2 : tyBool :-
+    Γ ⊢ T1 : tyNat,
+    Γ ⊢ T2 : tyNat.
+
+Γ ⊢ T1 < T2 : tyBool :-
+    Γ ⊢ T1 : tyNat,
+    Γ ⊢ T2 : tyNat.
+
+/* some shorthads for Natural Numbers */
+Γ ⊢ 1 : tyNat.
+Γ ⊢ 2 : tyNat.
+Γ ⊢ 3 : tyNat.
+Γ ⊢ 4 : tyNat.
+Γ ⊢ 5 : tyNat.
+
+/*
+Examples:
+
+term: fix lambda f. lambda x. if x < 0 || x == 0
+                              then 1
+                              else f (x - 1)
+query: [] ⊢ fix(lam(f, lam(x, if(or(var(x)<0, var(x)==0), 1, var(f) $ (var(x) - 1))))) : Ty.
+
+term: fix lambda f. lambda x. if x < 0 || x == 0
+                              then 0
+                              else if x == 1
+                                   then 1
+                                   else f (x - 1) + f (x - 2)
+query: [] ⊢ fix(lam(f, lam(x, if(or(var(x)<0, var(x)==0), 0, if(var(x)==1, 1, var(f) $ (var(x) - 1) + var(f) $ (var(x) - 2)))))) : Ty.
+*/
+
+/* Lists */
+
+Γ ⊢ nil : tyList(Ty).
+
+Γ ⊢ cons(T1, T2) : tyList(Ty) :-
+    Γ ⊢ T1 : Ty,
+    Γ ⊢ T2 : tyList(Ty).
+
+Γ ⊢ isNil(T) : tyBool :-
+    Γ ⊢ T : tyList(Ty).
+
+Γ ⊢ head(T) : Ty :-
+    Γ ⊢ T : tyList(Ty).
+
+Γ ⊢ tail(T) : tyList(Ty) :-
+    Γ ⊢ T : tyList(Ty).
+
+/*
+Examples:
+
+term: [1,2,3,4,5]
+query: [] ⊢ cons(1, cons(2, cons(3, cons(4, cons(5, nil))))) : Ty.
+
+term: fix lambda self. lambda f. lambda l. if (isNil l)
+                                           then nil
+                                           else cons (f (head l)) (self f (tail l))
+query: [] ⊢ fix(lam(self, lam(f, lam(l, if(isNil(var(l)), nil, cons(var(f) $ head(var(l)), var(self) $ var(f) $ tail(var(l)))))))) : Ty.
 */
 
 /* Let Polymorphism */
